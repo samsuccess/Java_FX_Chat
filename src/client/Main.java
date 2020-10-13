2b666c7c98328b82d6b7a44a1c57d89e0a22c725
samsuccess <samsuccess36@gmail.com>
2020-10-13 23:31:39 +0300

    change homework7

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Балабол");
+        primaryStage.setScene(new Scene(root, 450, 300));
         primaryStage.show();
     }
 
